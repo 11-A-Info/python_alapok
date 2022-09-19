92f540a68ce9e48e6d930a3cfd9b4aa4b1355088
pepe1973 <bodrogipr@gmail.com>
2022-09-19 10:36:10 +0200

    Negyedik commit

--- elso.py
+++ elso.py
@@ -2,3 +2,11 @@ from os import system
 
 system("cls")
 
+# 2. feladat: prog_alapf.3
+
+# explicit típuskonverzió
+szam = int(input("Kérek egy számot: "))
+kiir = f"A szám: {szam} és kétszerese: {2 * szam}"
+
+print(kiir)
+
---
+++ feladatok.py
@@ -0,0 +1,6 @@
+# 1. feladat: prog_alapf.2
+
+nev = input("Kérem a nevedet: ")
+koszones = f"Hello {nev}!"
+
+print(koszones)
